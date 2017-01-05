f260af1d159d8c15f152d1579a19a0e57c0d2cf8
Ericsen Garcia <ericsen.garcia@gmail.com>
2017-01-05 07:56:52 -0200

    inicio

---
+++ src/main/java/br/com/financeiro/util/DAOFactory.java
@@ -0,0 +1,14 @@
+package br.com.financeiro.util;
+
+import br.com.financeiro.usuario.UsuarioDAO;
+import br.com.financeiro.usuario.UsuarioDAOHibernate;
+
+public class DAOFactory {
+
+	public static UsuarioDAO criarUsuarioDAO() {
+		UsuarioDAOHibernate usuarioDAO = new UsuarioDAOHibernate();
+		usuarioDAO.setSession(HibernateUtil.getSessionfactory().getCurrentSession());
+		return usuarioDAO;
+	}
+
+}
---
+++ src/main/java/br/com/financeiro/web/UsuarioBean.java
@@ -0,0 +1,45 @@
+package br.com.financeiro.web;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import br.com.financeiro.usuario.Usuario;
+
+@ManagedBean(name = "usuarioBean")
+@RequestScoped
+public class UsuarioBean {
+	
+	private Usuario usuario = new Usuario();
+	private String confirmaSenha;
+	
+	
+	
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
+
+	public String getConfirmaSenha() {
+		return confirmaSenha;
+	}
+
+
+
+	public void setConfirmaSenha(String confirmaSenha) {
+		this.confirmaSenha = confirmaSenha;
+	}
+
+
+
+	public UsuarioBean() {		
+		
+	}
+
+}
